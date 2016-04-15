0a65ce28ad0b5cbf0071b6579e9533fcef08ce1c
Björn Dahlgren <bjodah@gmail.com>
2016-04-15 13:08:07 +0200

    Camilla's förbättringar

--- input/matlab/passning2.m
+++ input/matlab/passning2.m
@@ -16,16 +16,14 @@ g = @(p, x) (x.^p + 1);
 f = @(a, b, x) (a*g(b, x)./g(b+1, x));
 
 % Antag att f(t) är en observabel med mätosäkerhet
-% vi kan simulera detta med normalfördelat brus:
+% vi kan simulera detta med normalfördelat brus
+% (ni kommer istället att läsa in data från en textfil,
+% exempelvis mha. funktionen ``load``):
 N = 1000; t = linspace(0, 4, N);
 alpha = 2.0; beta = 3.0;
 rng('default'); % ger reproducerbara stokastiska värden
 y = f(alpha, beta, t) + random('Normal', 0, 0.1, 1, N);
 
-% Ni kommer istället att läsa in t och y från en textfil.
-% Det gör ni med funktionen ``load``, exempel:
-%
-%    raadata = load("XdegC_Ymolal_3.txt")
 
 % Låt oss plotta mätdata:
 plot(t, y);
@@ -34,6 +32,7 @@ plot(t, y);
 % bestämma alpha och beta från våra brusiga data i y.
 resfcn = @(params) f(params(1), params(2), t) - y;
 result = fsolve(resfcn, [1.0, 1.0])
+% [1.0, 1.0] är gissning för a och b, gissningen kan behöva uppdateras
 
 % vi kan rita in passningen över våra brusiga data för att
 % få en visualisering av "goodness of fit"
